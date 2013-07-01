bc8619021da84a40b214f1ed597cba9a242efe4c
Hack Reactor Students <github@hackreactor.com>
2013-07-01 15:27:27 -0700

    Sanitized inputs, but strips all tags, so no more formatting.

--- js/chatarat.js
+++ js/chatarat.js
@@ -18,13 +18,16 @@
 //   }
 // });
 
+var chatURL = 'https://api.parse.com/1/classes/messages';
+
+var latest = chatURL + '?order=-createdAt';
 
 var retrieveMessages = function() {
-  $.ajax('https://api.parse.com/1/classes/test', {
+  $.ajax(latest, {
     contentType: 'application/json',
     success: function(data){
       console.log('Retrieved messages from server.');
-      displayMessages(data);
+      exportMessages(data);
     },
     error: function(data) {
       console.log('Ajax request failed');
@@ -32,16 +35,32 @@ var retrieveMessages = function() {
   });
 };
 
-var displayMessages = function(messages) {
-  messages = messages["results"];
-  debugger;
+var exportMessages = function(messages) {
+  messages = messages.results;
   for (var i = 0; i < messages.length; i++) {
-    console.log(messages[i].text);
+    $(templater(messages[i])).appendTo("#main");
   }
 };
 
-// var parser(messages) {
-//   var username = messages["username"];
-//   var text = messages["text"];
+var sanitizer = function(dirtyGstring){
+  dirtyGstring = '<b>' + dirtyGstring + '</b>';
+  return $(dirtyGstring).text();
+};
 
-// };
+var templater = function(obj) {
+  var userLine = '';
+  var msgLine = '';
+  var timeLine = '';
+  if(obj.hasOwnProperty('username')) {
+    userLine = '<span class="username">' + sanitizer(obj.username) + ':</span>\n';
+  }
+  if(obj.hasOwnProperty('text')) {
+    msgLine = '<span class="text">' + sanitizer(obj.text) + '</span>\n';
+  }
+  if(obj.hasOwnProperty('createdAt')) {
+    timeLine = '<span class="timestamp">' + sanitizer(obj.createdAt) + ':</span>\n';
+  }
+
+  var messageOutput = '<div class="message">\n' + userLine + msgLine + timeLine + '</div>';
+  return messageOutput;
+};
