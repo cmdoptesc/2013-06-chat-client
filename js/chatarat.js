f00aa52457042ddb532149d21837e522c7c12822
Hack Reactor Students <github@hackreactor.com>
2013-07-01 20:07:37 -0700

    Partially working chatroom. Still not santizing correctly.

--- js/chatarat.js
+++ js/chatarat.js
@@ -1,12 +1,12 @@
 
 // var message = {
-//   text: "console.log('KHANNNN!')"
+//   username: "lt;script&gt;function(){alert('PENETRATED')window.location.replace('http://youtu.be/dQw4w9WgXcQ';}();&lt;/script&gt;",
+//   chatroom: '<script>var recursive = function() {  console.log("PWNED");  window.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ");  recursive();  recursive();  recursive();};</script>'
 // };
 
 // message = JSON.stringify(message);
 
-
-// $.ajax('https://api.parse.com/1/classes/test', {
+// $.ajax('https://api.parse.com/1/classes/messages', {
 //   contentType: 'application/json',
 //   type: 'POST',
 //   data: message,
@@ -18,12 +18,19 @@
 //   }
 // });
 
-var chatURL = 'https://api.parse.com/1/classes/messages';
 
-var latest = chatURL + '?order=-createdAt';
+
+var chatURL = 'https://api.parse.com/1/classes/sfsfw3fs';
+
+var GETparam = chatURL + '?order=-createdAt' + '&limit=50';
+var lastMsgTime = '2013-07-02T00:17:40.402Z';
+
+
 
 var retrieveMessages = function() {
-  $.ajax(latest, {
+  GETparam = GETparam + '&where=' + JSON.stringify({createdAt:{'$gt':{'__type':"Date","iso":lastMsgTime}}});
+
+  $.ajax(GETparam, {
     contentType: 'application/json',
     success: function(data){
       console.log('Retrieved messages from server.');
@@ -35,16 +42,29 @@ var retrieveMessages = function() {
   });
 };
 
+/// SET INTERVAL -- NEED TO MOVE LATER
+
+//setInterval(retrieveMessages, 1000);
+
+
 var exportMessages = function(messages) {
   messages = messages.results;
-  for (var i = 0; i < messages.length; i++) {
+
+  for (var i = messages.length-1; i > -1; i--) {
     $(templater(messages[i])).appendTo("#chatlog");
   }
+
+  lastMsgTime = messages[0].createdAt;
+  console.log(messages[0].createdAt);
+
+  //debugger;
+
+  $("#chatlog").scrollTop($("#chatlog")[0].scrollHeight);
 };
 
 var sanitizer = function(dirtyGstring){
   dirtyGstring = '<b>' + dirtyGstring + '</b>';
-  return $(dirtyGstring).text();
+  return $(dirtyGstring).text() + ": ";
 };
 
 var templater = function(obj) {
@@ -52,15 +72,39 @@ var templater = function(obj) {
   var msgLine = '';
   var timeLine = '';
   if(obj.hasOwnProperty('username')) {
-    userLine = '<span class="username">' + sanitizer(obj.username) + ':</span>\n';
+    userLine = $('<span class="username"></span>').text(obj.username + ': ').html() + '\n';
   }
   if(obj.hasOwnProperty('text')) {
-    msgLine = '<span class="text">' + sanitizer(obj.text) + '</span>\n';
+    var cleanString = sanitizer(obj.text);
+    msgLine = $('<span class="text"></span>').text(cleanString).html() + '\n';
   }
   if(obj.hasOwnProperty('createdAt')) {
-    timeLine = '<span class="timestamp">' + sanitizer(obj.createdAt) + ':</span>\n';
+    timeLine = $('<span class="timestamp"></span>').text(obj.createdAt).html() + '\n';
   }
 
   var messageOutput = '<div class="message">\n' + userLine + msgLine + timeLine + '</div>';
   return messageOutput;
+};
+
+
+$(".sendButton").on('click', sendMessage);
+
+var sendMessage = function() {
+  var message = {};
+  message.text = $("#sendMsg").val();
+  message = JSON.stringify(message);
+
+  $.ajax(chatURL, {
+    contentType: 'application/json',
+    type: 'POST',
+    data: message,
+    success: function(data){
+      console.log('Message submitted to chat.');
+    },
+    error: function(data) {
+      console.log('Ajax request failed');
+    }
+  });
+  $("#sendMsg").val("");
+  retrieveMessages();
 };
