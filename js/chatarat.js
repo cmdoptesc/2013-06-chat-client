4171534b26bc5828a246f2424fdb6adf23bfb835
Hack Reactor Students <github@hackreactor.com>
2013-07-02 15:55:05 -0700

    Added friends feature.

--- js/chatarat.js
+++ js/chatarat.js
@@ -1,7 +1,7 @@
 
 // var message = {
 //   username: "lt;script&gt;function(){alert('PENETRATED')window.location.replace('http://youtu.be/dQw4w9WgXcQ';}();&lt;/script&gt;",
-//   chatroom: '<script>var recursive = function() {  console.log("PWNED");  window.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ");  recursive();  recursive();  recursive();};</script>'
+//   text: '<script>var recursive = function() {  console.log("PWNED");  window.location.replace("http://www.youtube.com/watch?v=dQw4w9WgXcQ");  recursive();  recursive();  recursive();};</script>'
 // };
 
 // message = JSON.stringify(message);
@@ -18,14 +18,10 @@
 //   }
 // });
 
-
-
-var chatURL = 'https://api.parse.com/1/classes/sfsfw3fs';
+var chatURL = 'https://api.parse.com/1/classes/messages';
 
 var GETparam = chatURL + '?order=-createdAt' + '&limit=50';
 var lastMsgTime = '2013-07-02T00:17:40.402Z';
-
-
 
 var retrieveMessages = function() {
   GETparam = GETparam + '&where=' + JSON.stringify({createdAt:{'$gt':{'__type':"Date","iso":lastMsgTime}}});
@@ -53,9 +49,7 @@ var exportMessages = function(messages) {
   for (var i = messages.length-1; i > -1; i--) {
     templater(messages[i]).appendTo("#chatlog");
   }
-
   lastMsgTime = messages[0].createdAt;
-
 
   $("#chatlog").scrollTop($("#chatlog")[0].scrollHeight);
 };
@@ -64,11 +58,12 @@ var templater = function(obj) {
   var messageOutput = $('<div class="message"></div>');
 
   if(obj.hasOwnProperty('username')) {
-    $('<span class="username"></span>').text(obj.username + ': ').appendTo(messageOutput);
+    $('<a href="#" class="username '+obj.username+'"></a>').text(obj.username + ': ').appendTo(messageOutput);
   }
   if(obj.hasOwnProperty('text')) {
     //var cleanString = sanitizer(obj.text);
-    $("<span class='text'></span>").text(obj.text).appendTo(messageOutput);
+    var shortened = obj.text.substr(0,160);
+    $("<span class='text'></span>").text(shortened + ' ').appendTo(messageOutput);
   }
   if(obj.hasOwnProperty('createdAt')) {
     $('<span class="timestamp"></span>').text(obj.createdAt).appendTo(messageOutput);
@@ -77,25 +72,32 @@ var templater = function(obj) {
   return messageOutput;
 };
 
-
 $(".sendButton").on('click', sendMessage);
+
+$('#chatlog').on('click', 'a', function(){
+  var username = this.classList[1];
+  $('#chatlog').find('.'+username).next().toggleClass("friend");
+});
 
 var sendMessage = function() {
   var message = {};
+  message.username = _username;
   message.text = $("#sendMsg").val();
   message = JSON.stringify(message);
 
-  $.ajax(chatURL, {
-    contentType: 'application/json',
-    type: 'POST',
-    data: message,
-    success: function(data){
-      console.log('Message submitted to chat.');
-    },
-    error: function(data) {
-      console.log('Ajax request failed');
-    }
-  });
+  if($("#sendMsg").val() !== '') {
+    $.ajax(chatURL, {
+      contentType: 'application/json',
+      type: 'POST',
+      data: message,
+      success: function(data){
+        console.log('Message submitted to chat.');
+      },
+      error: function(data) {
+        console.log('Ajax request failed');
+      }
+    });
+  }
   $("#sendMsg").val("");
   retrieveMessages();
 };
--- js/setup.js
+++ js/setup.js
@@ -7,6 +7,16 @@ if(!/(&|\?)username=/.test(window.location.search)){
   window.location.search = newSearch;
 }
 
+var _username = '';
+
+if(window.location.search.substr(1,8)==='username') {
+  var stringEnd;
+  if (window.location.search.indexOf('&') !== -1) { stringEnd = window.location.search.indexOf('&'); }
+  else { stringEnd = window.location.search.length; }
+  _username = window.location.search.substring(10, stringEnd);
+  console.log(_username);
+}
+
 // Don't worry about this code, it will ensure that your ajax calls are allowed by the browser
 $.ajaxPrefilter(function(settings, _, jqXHR) {
   jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
