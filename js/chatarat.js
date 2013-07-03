fe5cc825a69b35ec06357bb3bd68e76ecf0869cb
Hack Reactor Students <github@hackreactor.com>
2013-07-02 17:52:48 -0700

    Finished with minimalist styling.

--- js/chatarat.js
+++ js/chatarat.js
@@ -1,6 +1,6 @@
 
 // var message = {
-//   username: "lt;script&gt;function(){alert('PENETRATED')window.location.replace('http://youtu.be/dQw4w9WgXcQ';}();&lt;/script&gt;",
+//   username: "lt;script&gt;function(){alert('PENETRATED');window.location.replace('http://youtu.be/dQw4w9WgXcQ';}();&lt;/script&gt;",
 //   text: '<script>var recursive = function() {  console.log("PWNED");  window.location.replace("http://www.youtube.com/watch?v=dQw4w9WgXcQ");  recursive();  recursive();  recursive();};</script>'
 // };
 
@@ -18,15 +18,16 @@
 //   }
 // });
 
-var chatURL = 'https://api.parse.com/1/classes/messages';
-
-var GETparam = chatURL + '?order=-createdAt' + '&limit=50';
-var lastMsgTime = '2013-07-02T00:17:40.402Z';
+var baseURL = 'https://api.parse.com/1/classes/';
+var _roomname = 'messages';
 
 var retrieveMessages = function() {
-  GETparam = GETparam + '&where=' + JSON.stringify({createdAt:{'$gt':{'__type':"Date","iso":lastMsgTime}}});
+  var getParam = '?order=-createdAt' + '&limit=50';
+  var lastMsgTime = '2013-07-02T00:17:40.402Z';
 
-  $.ajax(GETparam, {
+  var getURL = baseURL + _roomname + getParam + '&where=' + JSON.stringify({createdAt:{'$gt':{'__type':"Date","iso":lastMsgTime}}});
+
+  $.ajax(getURL, {
     contentType: 'application/json',
     success: function(data){
       console.log('Retrieved messages from server.');
@@ -39,18 +40,16 @@ var retrieveMessages = function() {
 };
 
 /// SET INTERVAL -- NEED TO MOVE LATER
-
 //setInterval(retrieveMessages, 1000);
 
 
 var exportMessages = function(messages) {
   messages = messages.results;
-
   for (var i = messages.length-1; i > -1; i--) {
     templater(messages[i]).appendTo("#chatlog");
   }
-  lastMsgTime = messages[0].createdAt;
 
+  lastMsgTime = messages[0].createdAt;
   $("#chatlog").scrollTop($("#chatlog")[0].scrollHeight);
 };
 
@@ -68,30 +67,25 @@ var templater = function(obj) {
   if(obj.hasOwnProperty('createdAt')) {
     $('<span class="timestamp"></span>').text(obj.createdAt).appendTo(messageOutput);
   }
-
   return messageOutput;
 };
-
-$(".sendButton").on('click', sendMessage);
-
-$('#chatlog').on('click', 'a', function(){
-  var username = this.classList[1];
-  $('#chatlog').find('.'+username).next().toggleClass("friend");
-});
 
 var sendMessage = function() {
   var message = {};
   message.username = _username;
+  message.roomname = _roomname;
   message.text = $("#sendMsg").val();
   message = JSON.stringify(message);
 
+  var postURL = baseURL + _roomname;
+
   if($("#sendMsg").val() !== '') {
-    $.ajax(chatURL, {
+    $.ajax(postURL, {
       contentType: 'application/json',
       type: 'POST',
       data: message,
       success: function(data){
-        console.log('Message submitted to chat.');
+        console.log('Message submitted to room ' + _roomname);
       },
       error: function(data) {
         console.log('Ajax request failed');
@@ -101,3 +95,27 @@ var sendMessage = function() {
   $("#sendMsg").val("");
   retrieveMessages();
 };
+
+var updateTitle = function() {
+  var title = 'chatarat';
+  (_roomname==='messages') ? title = 'the cesspool we call "messages"' : title = '"'+ _roomname + '"';
+  $('#roomTitle').text(title);
+};
+
+var changeRoom = function() {
+  _roomname = $("#roomChange").val();
+  updateTitle();
+  $('<div class="announcement">Now entering ' + _roomname + '</div>').appendTo('#chatlog');
+
+  $('#roomChange').val('');
+  retrieveMessages();
+};
+
+// running functions
+
+updateTitle();
+
+$('#chatlog').on('click', 'a', function(){
+  var username = this.classList[1];
+  $('#chatlog').find('.'+username).next().toggleClass("friend");
+});
